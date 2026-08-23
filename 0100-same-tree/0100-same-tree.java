92284deae7a4d69e26a4974fbfe3212a046d18ae
aafaq <aafaqhussain727@gmail.com>
2026-08-23 14:00:31 +0530

    Time: 0 ms (100%), Space: 42.6 MB (86.6%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,16 +1,8 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        String hash1 = computeTreeHash(p);
-        String hash2 = computeTreeHash(q);
-        return hash1.equals(hash2);
-    }
-    
-    private String computeTreeHash(TreeNode node) {
-        if (node == null) {
-            return "null";
-        }
-        String leftHash = computeTreeHash(node.left);
-        String rightHash = computeTreeHash(node.right);
-        return "(" + node.val + leftHash + rightHash + ")";
+        
+        if (p == null || q == null) { return p == q;}
+        
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
     }
 }
